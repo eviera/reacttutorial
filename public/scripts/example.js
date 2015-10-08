5ad2f52ce83491994f96189318187478cb2462df
eviera@sdd <eviera@sdd>
2015-10-08 17:06:51 -0300

    tut

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -2,11 +2,54 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-       Hello World commentBox
+       <h1>Comments</h1>
+       <CommentList />
+       <CommentForm />
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        soy commentForm
+      </div>
+    );
+  }
+});
+
+// tutorial7.js
+var Comment = React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
 
 React.render(
   <CommentBox/>,
